2a3547e24e1f5c24f08acf09566c6b8629ea23ce
Manjesh Vinayaka <manjesh@quetzal.in>
2016-03-04 09:14:18 +0530

    Improvements related to /home

--- server/config/environment/angular/development.js
+++ server/config/environment/angular/development.js
@@ -3,6 +3,8 @@
 exports = module.exports = {
   // List of user roles
   QUARC_API: 'http://api.quezx.dev/api',
+  HIRE: '//hire.quezx.dev',
+  PARTNER: '//partner.quezx.dev',
   OAUTH: '//accounts.quezx.dev/authorise?client_id=partnerquezx&response_type=code&' +
 'redirect_uri=http://partner.quezx.dev/access/oauth&state=yo',
 
